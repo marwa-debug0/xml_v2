cbff0882dfe423fd8fa5af7a7a7b17045459a38b
marwa_debug0 <marwaelatlassi2005@gmail.com>
2026-06-19 00:45:19 +0100

    Add files via upload

---
+++ src/main/java/com/example/xml_project/model/product.java
@@ -0,0 +1,20 @@
+package com.example.xml_project.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double price;
+}
